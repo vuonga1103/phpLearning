25738be1fc863f21a22fe081e430be03baff3247
vuonga1103 <vuonga1103@yahoo.com>
2021-01-02 16:37:55 -0500

    variables and constants

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<!-- Hello -->
+<?php
+// creating variables
+  $name = 'Anh';
+  $age = 27;
+
+// creating constants
+  define('PI', 3.14); // redefining 'PI' will cause an error
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>my first php file</title>
+</head>
+<body>
+
+  <h1><?php echo 'hello, world';?></h1>
+  <p><?php echo 'my name is ' . $name;?></p>
+  <p><?php echo 'i am ' . $age . ' years old'; ?> </p>
+  <p><?php echo 'the value of pi is ' . PI; ?></p>
+</body>
+</html>
